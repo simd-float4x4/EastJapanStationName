eb318303aa14d4b977b94bab2b48edbab1a85a4e
simd-float4x4 <asobu.developers+mathew@gmail.com>
2025-10-24 11:59:39 +0900

    横須賀線（JO）

---
+++ Sources/EastJapanStationName/Data/JO.swift
@@ -0,0 +1,78 @@
+//
+//  JO.swift
+//  EastJapanStationName
+//
+//  Created by Shumpei Horiuchi on 2025/10/24.
+//
+//　横須賀線
+//
+
+enum JO {
+    case JO01
+    case JO02
+    case JO03
+    case JO04
+    case JO05
+    case JO06
+    case JO07
+    case JO08
+    case JO09
+    case JO10
+    case JO11
+    case JO12
+    case JO13
+    case JO14
+    case JO15
+    case JO16
+    case JO17
+    case JO18
+    case JO19
+    
+    var stationNameKanji: String {
+        switch self {
+        case .JO01: return "久里浜"
+        case .JO02: return "衣笠"
+        case .JO03: return "横須賀"
+        case .JO04: return "田浦"
+        case .JO05: return "東逗子"
+        case .JO06: return "逗子"
+        case .JO07: return "鎌倉"
+        case .JO08: return "北鎌倉"
+        case .JO09: return "大船"
+        case .JO10: return "戸塚"
+        case .JO11: return "東戸塚"
+        case .JO12: return "保土ヶ谷"
+        case .JO13: return "横浜"
+        case .JO14: return "新川崎"
+        case .JO15: return "武蔵小杉"
+        case .JO16: return "西大井"
+        case .JO17: return "品川"
+        case .JO18: return "新橋"
+        case .JO19: return "東京"
+        }
+    }
+    
+    var stationNameRomaji: String {
+        switch self {
+        case .JO01: return "kurihama"
+        case .JO02: return "kinugasa"
+        case .JO03: return "yokosuka"
+        case .JO04: return "taura"
+        case .JO05: return "higashi-zushi"
+        case .JO06: return "zushi"
+        case .JO07: return "kamakura"
+        case .JO08: return "kita-kamakura"
+        case .JO09: return "ofuna"
+        case .JO10: return "totsuka"
+        case .JO11: return "higashitotsuka"
+        case .JO12: return "hodogaya"
+        case .JO13: return "yokohama"
+        case .JO14: return "shin-kawasaki"
+        case .JO15: return "musashi-kosugi"
+        case .JO16: return "nishi-oi"
+        case .JO17: return "shinagawa"
+        case .JO18: return "shimbashi"
+        case .JO19: return "tokyo"
+        }
+    }
+}
